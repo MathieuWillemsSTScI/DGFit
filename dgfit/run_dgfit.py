7bcc0e58f73071118c14ab68ee18857e9333bd06
Mathieu Willems <mwillems@mwgbpc.stsci.edu>
2026-05-15 12:28:25 -0400

    changing prior bounds

--- dgfit/run_dgfit.py
+++ dgfit/run_dgfit.py
@@ -717,7 +717,6 @@ def main():
                         upper /= new_factor_sil
                         lower /= new_factor_sil
                         start_value /= new_factor_sil
-                    upper /= 10 *len(dustmodel.components[k].sizes)
 
 
                 prior.add_parameter(
